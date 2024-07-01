c1b5932bd358662cd434450d7c7fc93a543949ed
Marharyta Salii <margarita12122015@gmail.com>
2024-07-01 19:38:02 +0300

    first commit

---
+++ src/test/java/mate/academy/store/OnlineBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
