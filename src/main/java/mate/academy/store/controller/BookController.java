dce9c9ad8ccc3bee5df21ad55de6a748ccb22c1e
Marharyta Salii <margarita12122015@gmail.com>
2024-07-17 15:53:09 +0300

    added-pagination-sorting-swagger

--- src/main/java/mate/academy/store/controller/BookController.java
+++ src/main/java/mate/academy/store/controller/BookController.java
@@ -1,5 +1,7 @@
 package mate.academy.store.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import java.util.List;
@@ -8,6 +10,7 @@ import mate.academy.store.dto.BookDto;
 import mate.academy.store.dto.BookSearchParameters;
 import mate.academy.store.dto.CreateBookRequestDto;
 import mate.academy.store.service.BookService;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Book management", description = "Endpoints for managing books")
 @RestController
 @RequestMapping("/books")
 @RequiredArgsConstructor
@@ -28,21 +32,26 @@ public class BookController {
     private final BookService bookService;
 
     @GetMapping
-    public List<BookDto> getAll() {
-        return bookService.findAll();
+    @Operation(summary = "Get all books", description = "Get a list of all available books")
+    public List<BookDto> getAll(Pageable pageable) {
+        return bookService.findAll(pageable);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get book by ID", description = "Get one book according to its ID")
     public BookDto getBookById(@PathVariable @Positive Long id) {
         return bookService.findById(id);
     }
 
     @PostMapping
+    @Operation(summary = "Create a new book",
+                description = "Create a new book and save it to the DB")
     public BookDto createBook(@RequestBody @Valid CreateBookRequestDto requestDto) {
         return bookService.save(requestDto);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update book", description = "Update an existing book from DB by its ID")
     public BookDto updateBook(@PathVariable @Positive Long id,
                               @RequestBody @Valid CreateBookRequestDto requestDto) {
         return bookService.updateById(id, requestDto);
@@ -50,11 +59,14 @@ public class BookController {
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete book", description = "Delete one book from DB according to its ID")
     public void delete(@PathVariable @Positive Long id) {
         bookService.deleteById(id);
     }
 
     @GetMapping("/search")
+    @Operation(summary = "Search book by parameters",
+                description = "Search book by specific parameters")
     public List<BookDto> search(@Valid BookSearchParameters searchParameters) {
         return bookService.search(searchParameters);
     }
--- src/main/java/mate/academy/store/service/BookService.java
+++ src/main/java/mate/academy/store/service/BookService.java
@@ -4,11 +4,12 @@ import java.util.List;
 import mate.academy.store.dto.BookDto;
 import mate.academy.store.dto.BookSearchParameters;
 import mate.academy.store.dto.CreateBookRequestDto;
+import org.springframework.data.domain.Pageable;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto requestDto);
 
-    List<BookDto> findAll();
+    List<BookDto> findAll(Pageable pageable);
 
     BookDto findById(Long id);
 
